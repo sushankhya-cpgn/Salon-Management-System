1a921a22f6413803a9dd53054a98028116061c79
Sushankhya Chapagain <chapagain.sushankhya@gmail.com>
2026-01-13 22:36:58 +0545

    Containerized postgres and server

--- src/controller/authController.ts
+++ src/controller/authController.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import { prisma } from "../lib/prisma.js";
+import {prisma} from "../lib/prisma.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
