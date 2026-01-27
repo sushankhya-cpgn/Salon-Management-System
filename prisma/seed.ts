a5b5c684a6e93732e102d67161be8fbd690d15ea
Sushankhya Chapagain <chapagain.sushankhya@gmail.com>
2026-01-27 23:50:59 +0545

    Added missing prices

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,9 +6,9 @@ console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
     await prisma.service.createMany({
         data:[
-            {name:"Haircut",duration:30},
-            {name:"Manicure",duration:45},
-            {name:"Spa",duration:60}
+            {name:"Haircut",duration:30,price:30.4},
+            {name:"Manicure",duration:45,price:40.4},
+            {name:"Spa",duration:60,price:50}
         ]
     })
 }
