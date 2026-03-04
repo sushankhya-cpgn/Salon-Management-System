50b2f7c37429f63a3d6cee47b485a0c7cf1dbda4
Sushankhya Chapagain <chapagain.sushankhya@gmail.com>
2026-03-05 00:25:28 +0545

    Register Form Validation added

--- Frontend/my-app/app/(auth)/layout.tsx
+++ Frontend/my-app/app/(auth)/layout.tsx
@@ -9,7 +9,7 @@ export default function AuthLayout({
   subtitle:string
 }) {
   return (
-    <div className=" h-screen w-screen flex items-center justify-center bg-gray-100 px-4">
+    <div className=" min-h-screen w-screen flex items-center justify-center bg-gray-100 px-4">
       <div className="w-3/4  h-3/4 bg-white rounded-2xl shadow-xl overflow-hidden grid md:grid-cols-2">
         {/* Left Image Section */}
         <div className="hidden md:block relative">
--- Frontend/my-app/components/register-form.tsx
+++ Frontend/my-app/components/register-form.tsx
@@ -3,15 +3,50 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import Link from "next/link";
 import { Eye, EyeClosed } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { RegisterInput, registerSchema } from "@/lib/validations/authSchema";
 
 export function RegisterForm() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterInput>({
+    resolver: zodResolver(registerSchema), // Integrate Zod resolver
+    defaultValues: {
+      email: "",
+      password: "",
+      name: "",
+    },
+  });
+
+  const onSubmit = (data: RegisterInput) => {
+    console.log(data); // Data is validated and type-safe here
+  };
 
   return (
-    <form className="mt-8 space-y-5">
+    <form
+      className="mt-8 space-y-5 overflow-y-scroll"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div>
+        <label className="text-sm font-medium text-gray-700">Name</label>
+        <Input
+          type="text"
+          placeholder="Enter your name"
+          className="mt-1"
+          value={name}
+          {...register("name")}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <p className=" text-sm text-red-500">{errors.name?.message}</p>
       <div>
         <label className="text-sm font-medium text-gray-700">Email</label>
         <Input
@@ -19,9 +54,11 @@ export function RegisterForm() {
           placeholder="Enter your email"
           className="mt-1"
           value={email}
+          {...register("email")}
           onChange={(e) => setEmail(e.target.value)}
         />
       </div>
+      <p className=" text-sm text-red-500">{errors.email?.message}</p>
 
       <div>
         <label className="text-sm font-medium text-gray-700">Password</label>
@@ -31,13 +68,18 @@ export function RegisterForm() {
             placeholder="Enter your password"
             className="mt-1"
             value={password}
+            {...register("password")}
             onChange={(e) => setPassword(e.target.value)}
           />
-          <div className="flex justify-center items-center px-2 absolute right-0 h-full cursor-pointer">
+          <div
+            className="flex justify-center items-center px-2 absolute right-0 h-full cursor-pointer"
+            onClick={() => setShowPassword((prev) => !prev)}
+          >
             {!showPassword ? <Eye /> : <EyeClosed />}
           </div>
         </div>
       </div>
+      <p className=" text-sm text-red-500">{errors.password?.message}</p>
       <div>
         <label className="text-sm font-medium text-gray-700">
           Confirm Password
@@ -47,11 +89,13 @@ export function RegisterForm() {
           placeholder="Enter your password"
           className="mt-1"
           value={confirmPassword}
+          {...register("confirmPassword")}
           onChange={(e) => setConfirmPassword(e.target.value)}
         />
       </div>
+      <p className=" text-sm text-red-500">{errors.confirmPassword?.message}</p>
 
-      <Button className="w-full mt-4" size="lg">
+      <Button type="submit" className="w-full mt-4" size="lg">
         Register
       </Button>
 
